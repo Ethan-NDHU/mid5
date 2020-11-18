6ca02d92bc9b6d1c310ae3ab636b5fdfd04d53d8
410696016_Ethan <71751245+Ethan-NDHU@users.noreply.github.com>
2020-11-18 15:52:29 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.forever(function () {
+	
+})
